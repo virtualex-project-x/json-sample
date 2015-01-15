270697ad35a29cd433318c56705650173a1532ab
jun-nakashima <junnakashima@gmail.com>
2015-01-15 22:21:55 +0900

    [modify]http通信をvolleyフレームワーク経由に修正

--- app/src/main/java/com/example/n_suzuki/jsonsample/MainActivity.java
+++ app/src/main/java/com/example/n_suzuki/jsonsample/MainActivity.java
@@ -4,19 +4,75 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends ActionBarActivity {
+
+  private RequestQueue mQueue;
+
+  private Response.Listener<JSONArray> mListener= new Response.Listener<JSONArray>() {
+    @Override
+    public void onResponse(JSONArray jsonArrayResponse){
+      ArrayAdapter<String> arrayAdapter =
+          new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1);
+
+      try {
+        int cnt = jsonArrayResponse.length();
+        List<JSONObject> jsonList = new ArrayList<>();
+        for (int i = 0; i < cnt; i++) {
+          jsonList.add(jsonArrayResponse.getJSONObject(i));
+        }
+        for (JSONObject jsonObject : jsonList) {
+          String item = jsonObject.getString("content");
+          arrayAdapter.add(item);
+        }
+      } catch (JSONException e) {
+        e.printStackTrace();
+      }
+
+      ListView listView = (ListView) findViewById(R.id.jsonlistview);
+      listView.setAdapter(arrayAdapter);
+    }
+  };
+
+  private Response.ErrorListener mErrorListener = new Response.ErrorListener() {
+    @Override
+    public void onErrorResponse(VolleyError error) {
+      // 処理記載
+    }
+  };
+
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
-    //
-    AsyncHttpRequest task = new AsyncHttpRequest(this);
-    task.execute();
-  }
+    // AsyncTaskを使用したHTTPリクエスト
+    // AsyncHttpRequest task = new AsyncHttpRequest(this);
+    // task.execute();
 
+    // Volleyを使用したHTTPリクエスト
+    String url = "http://1.latest.slim3demo001.appspot.com/AjaxTweet.json";
+
+    mQueue = Volley.newRequestQueue(this);
+    mQueue.add(new JsonArrayRequest(url,  mListener, mErrorListener));
+
+  }
 
   @Override
   public boolean onCreateOptionsMenu(Menu menu) {
@@ -40,3 +96,5 @@ public class MainActivity extends ActionBarActivity {
     return super.onOptionsItemSelected(item);
   }
 }
+
+
